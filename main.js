3774d2cddfc355ad1e1f3b4e3ee0b00add77bbae
”Kodoku” <kodokunawa@gmail.com>
2023-06-03 19:44:18 +0300

    Исправлены анимации ховеров, удалены ненужные css свойства

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
-if(window.innerWidth  < 1080){
-    const burger = document.querySelector('.intro__menu-container');
-    const popup = document.querySelector('.popup');
-    const closeBtn = popup.querySelector('.close-popup');
+const burger = document.querySelector('.intro__menu-container');
+const popup = document.querySelector('.popup');
 
-    burger.addEventListener('click', () => popup.classList.add('active'));
+burger.addEventListener('click', () => popup.classList.add('active'));
+popup.addEventListener('click', function(evt){
+    const target = evt.target;
+    //закрытие попапа после нажатия на ссылку
+    if(target.classList.contains('nav-item')){
+        popup.classList.remove('active');
+    }
+});
 
-    popup.addEventListener('click', function(evt){
-        const target = evt.target;
-        if(target.classList.contains('nav-item')){
-            popup.classList.remove('active');
-        }
-    });
-
-    closeBtn.addEventListener('click', () => popup.classList.remove('active'));
-}
+const closeBtn = popup.querySelector('.close-popup-btn');
+closeBtn.addEventListener('click', () => popup.classList.remove('active'));
